694fca2a1d80168a359b6ff7283ee7ab3ac8713b
azoulais <74101852+azoulais@users.noreply.github.com>
2020-11-10 12:32:15 +0200

    Add files via upload

--- Tree.cpp
+++ Tree.cpp
@@ -1,89 +1,150 @@
-//
 // Created by spl211 on 05/11/2020.
-//
 
 #include "Tree.h"
 
-// class Tree
+// *************************************  class Tree  **************************
+Tree::Tree(int rootLabel) : node(rootLabel) {}        //CONSTRUCTOR
 
-//DEFAULT CONSTRUCTOR
-Tree::Tree(int rootLabel) {
-    this->node=rootLabel;
-}
-
-//COPY CONSTRUCTOR
+// Rule of 3
 Tree::Tree(const Tree &treeToCopy) {
-    treeToCopy.copy();
-}
-
-//COPY ASSIGNMENT OPERATOR
+    node = treeToCopy.node;
+    for (int i = 0; i < treeToCopy.children.size(); ++i) {
+        children.push_back(treeToCopy.children[i]->clone());
+    }
+}               //COPY CONSTRUCTOR
+Tree::~Tree() {
+    for (int i = 0; i < children.size(); ++i) {
+        delete children[i];
+    }
+}                                    //DESTRUCTOR
 Tree &Tree::operator=(const Tree &treeToCopy) {
-    if (this == &treeToCopy)
-        return *this;
+    if (this != &treeToCopy) {
+        node = treeToCopy.node;
+
+        for (int i = 0; i < children.size(); ++i) {         //destructing all 'my' children
+            delete children[i];
+        }
+        children.clear();
+        for (int i = 0; i < treeToCopy.children.size(); ++i) {
+            children.push_back(treeToCopy.children[i]->clone());
+        }
+    }
 
     return *this;
-}
+}       //COPY ASSIGNMENT OPERATOR
 
-//DESTRUCTOR
-Tree::~Tree() {
+//Rule of 5
+Tree::Tree(Tree &&treeToCopy) {
+    node=treeToCopy.node;
+    for (int i = 0; i < treeToCopy.children.size(); ++i) {
+        children.push_back(treeToCopy.children[i]);
+        treeToCopy.children[i]= nullptr;
+    }
+    treeToCopy.children.clear();
+}                   // MOVE CONSTRUCTOR
+const Tree &Tree::operator=(Tree &&treeToCopy) {
+    node =treeToCopy.node;
+    for (int i = 0; i < children.size(); ++i) {         //destructing all 'my' children
+        delete children[i];
+    }
     children.clear();
+
+    for (int i = 0; i < treeToCopy.children.size(); ++i) {
+        children.push_back(children[i]);
+    }
+
+    return *this;
+}   // MOVE ASSIGNMENT OPERATOR
+
+
+void Tree::addChild(const Tree &child) {
+     Tree* toAdd = child.clone();
+
+     int n= child.getNode();
+     std::vector<Tree*> newVec;
+     int i=0;
+     bool found = false;
+
+
+
+
+         for (i = 0; i < children.size() && !found; ++i) {
+             if ()
+
+                 if (n <= children[i]->getNode())
+                     found = true;
+         }
+         if (n >= children[children.size() - 1]->getNode())
+             i = children.size();
+         else
+             i--;
+
+
+    children.push_back(nullptr);
+
+    for (int j = children.size()-2; j > i; --j) {
+        children[j]=children[j+1];
+        if(j==i)
+
+    }
+
+    //  this->children.push_back((Tree*) (child));  //todo
 }
 
-void Tree::addChild(const Tree& child) {
-   // Tree toadd(child);
-  //  this->children.push_back((Tree*) (child));  //todo
-}
-void Tree::addChild(Tree* child) {
-    this->children.push_back((Tree*) (child));  //todo
+void Tree::addChild(Tree *child) {
+    this->children.push_back((Tree *) (child));  //todo
 }
 
-Tree * Tree::createTree(const Session &session, int rootLabel) {}
+
+Tree *Tree::createTree(const Session &session, int rootLabel) {}
 
 int Tree::traceTree() {}
 
-Tree * Tree::copy() const {
-    Tree a = new Tree(4);
+
+//********************* class CycleTree: public Tree ****************************
+CycleTree::CycleTree(int _rootLabel, int _currCycle) : Tree(_rootLabel), currCycle(_currCycle) {}   //CONSTRUCTOR
+// Rule of 3
+CycleTree::CycleTree(const CycleTree &cycleTreeToCopy) : Tree(cycleTreeToCopy) {
+    currCycle = cycleTreeToCopy.currCycle;
+}         //COPY CONSTRUCTOR
+CycleTree::~CycleTree() {
+
+}               //DESTRUCTOR??
+CycleTree &CycleTree::operator=(const CycleTree &cycleTreeToCopy) {
+    Tree *temp = cycleTreeToCopy.clone();
+}           //COPY ASSIGNMENT OPERATOR
+
+
+Tree *CycleTree::clone() const {
+    return new CycleTree(*this);
 }
 
 
-
-
-
-// class CycleTree: public Tree
-
-//DEFAULT CONSTRUCTOR
-CycleTree::CycleTree(int rootLabel, int currCycle)
-:Tree(rootLabel) {
-    //    return *this;
-}
-
-//COPY CONSTRUCTOR
-CycleTree::CycleTree(const CycleTree &cycleTreeToCopy)
-:Tree(cycleTreeToCopy){
-    //return *this;
-}
-
-//COPY ASSIGNMENT OPERATOR
-CycleTree & CycleTree::operator=(const CycleTree &cycleTreeToCopy) {
-
-}
-//DESTRUCTOR
-CycleTree::~CycleTree() noexcept {
+// ************************  class MaxRankTree: public Tree ***********************
+MaxRankTree::MaxRankTree(int rootLabel) : Tree(rootLabel) {}         // CONSTRUCTOR
+MaxRankTree::MaxRankTree(const CycleTree &cycleTreeToCopy) : Tree(cycleTreeToCopy) {
 
 }
 
-
-
-Tree * CycleTree::clone() {
-    CycleTree temp = new CycleTree(this->node,currCycle); //todo
-            int i= this.
-}
-
-// class MaxRankTree: public Tree
-Tree * MaxRankTree::clone() {
+CycleTree &MaxRankTree::operator=(const CycleTree &cycleTreeToCopy) {
 
 }
-// class RootTree: public Tree
-Tree * RootTree::clone() {
 
+Tree *MaxRankTree::clone() const {
+    return new MaxRankTree(*this);
 }
+
+// ************************  class RootTree: public Tree ****************************
+RootTree::RootTree(int rootLabel) : Tree(rootLabel) {}         // CONSTRUCTOR
+
+
+Tree *RootTree::clone() const {
+    return new RootTree(*this);
+}
+
+int Tree::getNode() const {
+    return this->node;
+};
+
+
+
--- Tree.h
+++ Tree.h
@@ -5,49 +5,78 @@
 
 class Session;
 
-class Tree{
+class Tree {
 public:
-    Tree(int rootLabel); //DEFAULT CONSTRUCTOR
-    Tree(const Tree &treeToCopy); //COPY CONSTRUCTOR
-    Tree &operator=(const Tree& treeToCopy);  //COPY ASSIGNMENT OPERATOR
-    virtual ~Tree();   //DESTRUCTOR
+    Tree(int rootLabel);                         //CONSTRUCTOR
+    Tree(const Tree &treeToCopy);               //COPY CONSTRUCTOR
+    virtual ~Tree();                           //DESTRUCTOR
+    Tree &operator=(const Tree &treeToCopy);  //COPY ASSIGNMENT OPERATOR
+    const Tree &operator=(Tree &&treeToCopy);       // MOVE ASSIGNMENT OPERATOR
+    Tree(Tree&& treeToCopy);                       // MOVE CONSTRUCTOR
 
-    void addChild(const Tree& child);
-    void addChild( Tree* child);
-    static Tree* createTree(const Session& session, int rootLabel);
-    virtual int traceTree()=0;
-    virtual Tree* clone()=0;
+
+
+
+
+
+    void addChild(const Tree &child);
+
+    void addChild(Tree *child);
+
+    int getNode() const;
+
+    static Tree *createTree(const Session &session, int rootLabel);
+
+    virtual int traceTree() = 0;
+
+    virtual Tree *clone() const = 0;
+
 
 private:
     int node;
-    std::vector<Tree*> children;
-    Tree* copy() const;     //todo implement
+    std::vector<Tree *> children;
 };
 
-class CycleTree: public Tree{
+class CycleTree : public Tree {
 public:
     CycleTree(int rootLabel, int currCycle); //DEFAULT CONSTRUCTOR
     CycleTree(const CycleTree &cycleTreeToCopy); //COPY CONSTRUCTOR
-    CycleTree &operator=(const CycleTree& cycleTreeToCopy);  //COPY ASSIGNMENT OPERATOR
+    CycleTree &operator=(const CycleTree &cycleTreeToCopy);  //COPY ASSIGNMENT OPERATOR
     virtual ~CycleTree();   //DESTRUCTOR
     virtual int traceTree();
-    virtual Tree* clone();
+
+    virtual Tree *clone() const;
+
+
 private:
     int currCycle;
+
 };
 
-class MaxRankTree: public Tree{
+class MaxRankTree : public Tree {
 public:
     MaxRankTree(int rootLabel);
+
+    MaxRankTree(const CycleTree &cycleTreeToCopy); //COPY CONSTRUCTOR
+    CycleTree &operator=(const CycleTree &cycleTreeToCopy);  //COPY ASSIGNMENT OPERATOR
+    //No need for destructor
+
     virtual int traceTree();
-    virtual Tree* clone();
+
+    virtual Tree *clone() const;
 };
 
-class RootTree: public Tree{
+class RootTree : public Tree {
 public:
     RootTree(int rootLabel);
+
+    RootTree(const CycleTree &cycleTreeToCopy); //COPY CONSTRUCTOR
+    CycleTree &operator=(const CycleTree &cycleTreeToCopy);  //COPY ASSIGNMENT OPERATOR
+    //No need for destructor
+
     virtual int traceTree();
-    virtual Tree* clone();
+
+    virtual Tree *clone() const;
 };
 
 #endif
